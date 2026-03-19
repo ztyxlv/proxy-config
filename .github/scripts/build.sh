e83ebfeb89f92967ba7d49d6b9f0a2979fef6572
ztyxlv <ztyxlv@users.noreply.github.com>
2026-03-19 22:21:30 +0800

    Updated on 2026-03-19 at 22:20:28

--- .github/scripts/build.sh
+++ .github/scripts/build.sh
@@ -28,29 +28,16 @@ install_mihomo() {
   sudo chmod +x /usr/local/bin/mihomo
 }
 
-# install_singbox() {
-#   local url
-
-#   url=$(curl -fsSL https://api.github.com/repos/SagerNet/sing-box/releases/latest \
-#     | yq -r '.assets[] | select(.name | test("sing-box-[0-9.]+-linux-amd64\\.tar\\.gz$")) | .browser_download_url')
-
-#   curl -fsSL "$url" \
-#     | tar -xz --strip-components=1 --wildcards '*/sing-box' -C /tmp
-
-#   sudo mv /tmp/sing-box /usr/local/bin/sing-box
-#   sudo chmod +x /usr/local/bin/sing-box
-# }
-
 install_singbox() {
   local url
 
   url=$(curl -fsSL https://api.github.com/repos/SagerNet/sing-box/releases/latest \
-    | jq -r '.assets[] | select(.name | test("^sing-box-[^-]+-linux-amd64\\.tar\\.gz$")) | .browser_download_url')
+    | yq -r '.assets[] | select(.name | test("sing-box-[0-9.]+-linux-amd64\\.tar\\.gz$")) | .browser_download_url')
 
-  echo "url: $url"
+  rm -rf /tmp/sing-box
 
   curl -fsSL "$url" \
-    | tar -xzv --strip-components=1 --wildcards '*/sing-box' -C /tmp
+    | tar -xz --strip-components=1 --wildcards '*/sing-box' -C /tmp
 
   sudo mv /tmp/sing-box /usr/local/bin/sing-box
   sudo chmod +x /usr/local/bin/sing-box
